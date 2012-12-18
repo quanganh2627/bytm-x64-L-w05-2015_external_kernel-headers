cfde9078e20a53bac8f8e4604b2db6b2b6ff222a
Hu Gang <gang.a.hu@intel.com>
2012-12-18 10:55:43 +0800

    atomisp: Update atomisp header for V4L2_CID_2A_STATUS
    BZ: 59516
    
    Added new IOCTL, V4L2_CID_2A_STATUS
    to allow querying of 2A status of the sensor.
    
    Change-Id: I920746389d3ef67b4a240acdea203800e8d89939
    Signed-off-by: Hu Gang <gang.a.hu@intel.com>
    Reviewed-on: http://android.intel.com:8080/82260
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- original/linux/atomisp.h
+++ original/linux/atomisp.h
@@ -503,7 +503,8 @@ enum atomisp_acc_memory {
 	ATOMISP_ACC_MEMORY_DMEM,
 	ATOMISP_ACC_MEMORY_VMEM,
 	ATOMISP_ACC_MEMORY_VAMEM1,
-	ATOMISP_ACC_MEMORY_VAMEM2
+	ATOMISP_ACC_MEMORY_VAMEM2,
+	ATOMISP_ACC_NR_MEMORY		/* Must be last */
 };
 
 struct atomisp_sp_arg {
@@ -544,6 +545,30 @@ struct atomisp_acc_fw_load {
 	unsigned int fw_handle;
 	void __user *data;
 };
+
+/*
+ * Load firmware to specified pipeline.
+ * For CSS 1.5 only.
+ */
+struct atomisp_acc_fw_load_to_pipe {
+	__u32 flags;			/* Flags, see below for valid values */
+	unsigned int fw_handle;		/* Handle, filled by kernel. */
+	__u32 size;			/* Firmware binary size */
+	void __user *data;		/* Pointer to firmware */
+	__u32 type;			/* Binary type */
+	__u32 reserved[3];		/* Set to zero */
+};
+
+#define ATOMISP_ACC_FW_LOAD_FL_PREVIEW		(1 << 0)
+#define ATOMISP_ACC_FW_LOAD_FL_COPY		(1 << 1)
+#define ATOMISP_ACC_FW_LOAD_FL_VIDEO		(1 << 2)
+#define ATOMISP_ACC_FW_LOAD_FL_CAPTURE		(1 << 3)
+#define ATOMISP_ACC_FW_LOAD_FL_ACC		(1 << 4)
+
+#define ATOMISP_ACC_FW_LOAD_TYPE_NONE		0 /* Normal binary: don't use */
+#define ATOMISP_ACC_FW_LOAD_TYPE_OUTPUT		1 /* Stage on output */
+#define ATOMISP_ACC_FW_LOAD_TYPE_VIEWFINDER	2 /* Stage on viewfinder */
+#define ATOMISP_ACC_FW_LOAD_TYPE_STANDALONE	3 /* Stand-alone acceleration */
 
 struct atomisp_acc_map {
 	__u32 flags;			/* Flags, see list below */
@@ -761,6 +786,9 @@ struct v4l2_private_int_data {
 #define ATOMISP_IOC_S_CONT_CAPTURE_CONFIG \
 	_IOWR('v', BASE_VIDIOC_PRIVATE + 62, struct atomisp_cont_capture_conf)
 
+#define ATOMISP_IOC_ACC_LOAD_TO_PIPE \
+	_IOWR('v', BASE_VIDIOC_PRIVATE + 63, struct atomisp_acc_fw_load_to_pipe)
+
 /*  ISP Private control IDs */
 #define V4L2_CID_ATOMISP_BAD_PIXEL_DETECTION \
 	(V4L2_CID_PRIVATE_BASE + 0)
@@ -811,6 +839,11 @@ struct v4l2_private_int_data {
 
 /* number of frames to skip at stream start */
 #define V4L2_CID_G_SKIP_FRAMES		   (V4L2_CID_CAMERA_LASTP1 + 17)
+
+/* Query sensor's 2A status */
+#define V4L2_CID_2A_STATUS                 (V4L2_CID_CAMERA_LASTP1 + 18)
+#define V4L2_2A_STATUS_AE_READY            (1 << 0)
+#define V4L2_2A_STATUS_AWB_READY           (1 << 1)
 
 #define V4L2_BUF_FLAG_BUFFER_INVALID       0x0400
 #define V4L2_BUF_FLAG_BUFFER_VALID         0x0800
