8186ed75d56c5353fa3b7e4d046fd29d6df8a06a
Wang Kun <kun.k.wang@intel.com>
2013-03-08 17:49:23 +0800

    Update the VSP header file to support VIED 1.10 release.
    BZ: 91558
    
    FRC 1.25x and tiling interfaces are supported
    Remove the unused definitions
    
    Change-Id: I3f3df09fa0a8414ddec9930b9fbe4308762265ba
    Signed-off-by: Wang Kun <kun.k.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/95751
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Wang, Lili A <lili.a.wang@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- original/linux/vsp_fw.h
+++ original/linux/vsp_fw.h
@@ -51,7 +51,8 @@ enum VssFrcQuality {
 enum VssFrcConversionRate {
 	VssFrc2xConversionRate,
 	VssFrc2_5xConversionRate,
-	VssFrc4xConversionRate
+	VssFrc4xConversionRate,
+	VssFrc1_25xConversionRate
 };
 
 struct VssProcPipelineParameterBuffer {
@@ -115,7 +116,12 @@ struct VssProcPicture {
 	unsigned int width;
 	unsigned int rot_angle;
 	unsigned int stride;
+	/* frame raw format */
 	unsigned int format;
+	/* flag indicating if frame is stored in tiled format */
+	unsigned int tiled;
+	/* to make multiple of 32 bytes*/
+	int _pad[7];
 };
 
 struct VssProcPictureParameterBuffer {
@@ -147,64 +153,6 @@ enum VssProcCommandType {
 	VspSetContextCommand =                    0xEBED
 };
 
-#define VSP_CMD_QUEUE_SIZE (64)
-#define VSP_ACK_QUEUE_SIZE (64)
-
-/*
- * Command types and data structure.
- * Each command has a type. Depending on the type there is some kind
- * of data in external memory,
- * The VSS will use its DMA to load data from the buffer into local memory.
- */
-struct vss_command_t {
-	unsigned int       context;
-	unsigned int       type;
-	unsigned int       buffer;
-	unsigned int       size;
-	unsigned int       buffer_id;
-	unsigned int       irq;
-	unsigned int       reserved6;
-	unsigned int       reserved7;
-};
-
-struct vss_response_t {
-	unsigned int       context;
-	unsigned int       type;
-	unsigned int       buffer;
-	unsigned int       size;
-	unsigned int       vss_cc;
-	unsigned int       reserved5;
-	unsigned int       reserved6;
-	unsigned int       reserved7;
-};
-
-/* Default initial values for vsp-command and vsp-response
-* Using those avoids the risk of uninitialized warnings when
-* the definition changes.
-*/
-#define VSP_COMMAND_INITIALIZER {0, 0, 0, 0, 0, 0, 0, 0}
-#define VSP_RESPONSE_INITIALIZER {0, 0, 0, 0, 0, 0, 0, 0}
-
-/*
- * Response types
- */
-enum VssResponseType {
-	VssCommandBufferReadyResponse,
-	VssInputSurfaceReadyResponse,
-	VssOutputSurfaceReadyResponse,
-	VssOutputSurfaceFreeResponse,
-	VssOutputSurfaceCrcResponse,
-	VssEndOfSequenceResponse,
-	VssErrorResponse,
-	VssIdleResponse
-};
-
-enum VssStatus {
-	VssOK,
-	VssInvalidCommandType,
-	VssInvalidCommandArgument,
-	VssInvalidProcPictureCommand
-};
 
 enum vsp_format {
 	VSP_NV12,
@@ -215,141 +163,6 @@ enum vsp_format {
 	VSP_NV16,
 	VSP_IYUV,
 	VSP_TYPE_ERROR
-};
-
-struct vsp_data {
-	unsigned int fw_state;
-	unsigned int uninit_req;
-};
-
-/**
- * Magic number to verify if firmware blob header matches the expected
- * firmware format. This number is both put in the firmware-blob-header
- * as well as shipped with the VSP-API-code.
- */
-#define VSP_FIRMWARE_MAGIC_NUMBER 0x45BF1838
-
-/**
- * Maximum number of programs supported by firmware blob header.
- * Note: changing this value requires an update to
- * VSP_FIRMWARE_MAGIC_NUMBER (to indicate a new firmware format)
- */
-#define VSP_MAX_PROGRAMS          32
-
-/**
- * Maximum size of string-table. Could require an update when more programs
- * or tables are added or longer program names are used.
- * Note: changing this value requires an update to
- * VSP_FIRMWARE_MAGIC_NUMBER (to indicate a new firmware format)
- */
-#define VSP_STRING_TABLE_SIZE     256
-
-enum vsp_processor {
-	vsp_sp0 = 0,
-	vsp_sp1 = 1,
-	vsp_vp0 = 2,
-	vsp_vp1 = 3,
-	vsp_mea = 4
-};
-
-/**
-* Struct containing information from the host for the firmware boot.
-* The firmware will receive the information in this struct through
-* the firmware header of the blob.
-*/
-struct vsp_config {
-	/* Magic number to verify if firmware blob header matches
-	   the expected firmware format
-	 */
-	unsigned int magic_number;
-	unsigned int num_programs;
-	/* array of program offsets */
-	unsigned int program_offset[VSP_MAX_PROGRAMS];
-	/* offsets in string table */
-	unsigned int program_name_offset[VSP_MAX_PROGRAMS];
-	char string_table[VSP_STRING_TABLE_SIZE];
-
-	/* boot program info */
-	unsigned int boot_processor;
-	unsigned int api_processor;
-
-	/* start of the boot-program code
-	 * add the firmware addr to this offset to get the iCache
-	 * base address for boot-processor
-	 */
-	unsigned int text_src;
-
-	/* PC of init entry function */
-	unsigned int init_addr;
-	/* PC of main entry function */
-	unsigned int main_addr;
-};
-
-struct vsp_ctrl_reg {
-	/* VSP_FIRMWARE_ADDR_REG */
-	unsigned int firmware_addr;
-
-	/* VSP_SETTINGS_ADDR_REG */
-	unsigned int setting_addr;
-
-	unsigned int reserved4;
-
-	union {
-		/* VSP_ENTRY_KIND_REG */
-		unsigned int entry_kind;
-
-		/* VSP_UNINIT_REQ_REG */
-		unsigned int uninit_req;
-	};
-
-	/* The following context not been used */
-	unsigned int context_init_req;
-	unsigned int context_init_ack;
-	unsigned int context_uninit_req;
-	unsigned int context_uninit_ack;
-	unsigned int context_buf_addr;
-	unsigned int context_buf_sz;
-
-	/* VSP_CMD_QUEUE_RD_REG */
-	unsigned int cmd_rd;
-
-	/* VSP_CMD_QUEUE_WR_REG */
-	unsigned int cmd_wr;
-
-	/* VSP_ACK_QUEUE_RD_REG */
-	unsigned int ack_rd;
-
-	/* VSP_ACK_QUEUE_WR_REG */
-	unsigned int ack_wr;
-};
-
-/* constant parameters passed from host to firmware,
- * address of this struct is passed via config reg
- * struct is written to ddr in vsp_init call, destroyed upon uninit
- */
-struct vsp_settings_t {
-	unsigned int firmware_addr;
-	unsigned int command_queue_size;
-	unsigned int command_queue_addr;
-	unsigned int response_queue_size;
-	unsigned int response_queue_addr;
-	unsigned int state_buffer_size;
-	unsigned int state_buffer_addr;
-	/* Extra field to align to 256 bit (for DMA) */
-	unsigned int reserved7;
-};
-
-/* default initializer to initialize vsp_settings struct
- * (including the extra alignment fields)
- */
-#define VSP_SETTINGS_INITIALIZER {0, 0, 0, 0, 0, 0, 0, 0}
-
-/* values passed via VSP_ENTRY_TYPE_REG */
-enum vsp_entry_kind {
-	vsp_entry_init     = 0,
-	vsp_entry_continue = 1,
-	vsp_entry_resume   = 2,
-	vsp_exit           = 3
 };
 
 #endif
